d10028e2e2b9e114aa58ca006bbee930df532411
nimeshdh <nimeshdhakal508@gmail.com>
2025-07-10 13:48:40 +0545

    practice code

--- day1.py
+++ day1.py
@@ -19,4 +19,24 @@
 
 
 
+# day 2 of prof class 
+# # slicing
+# text ='nimesh'   
+# # print(text[:-2])
+
+
+# # length 
+# print(len(text))
+
+#find and replace
+name= "global"
+print(name.find("g"))
+replaced=name.replace("g","p")
+print(replaced)
+
+# escape sequence character
+text="my name is nimesh \n and age is 20"  #\n bhaneko next line
+tex="my name \n\t is nimesh \n and age is 20" #\t spacing dinxa dherai by 4 ko space dincha
+print (text)
+print (tex)
 
---
+++ task.py
@@ -0,0 +1,16 @@
+#wap to detect double spaces in a string
+# text = input("Enter any string: ")
+
+# if "  " in text:
+#     print("Double spaces in the string")
+# else:
+#     print("No double spaces")
+
+
+#replace the double spaces from problem 3 with single letter
+text = input("Enter any string: ")
+
+new_text = text.replace("  ", " ")
+
+print("String after replacing double spaces:")
+print(new_text)
